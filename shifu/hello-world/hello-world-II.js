6523ec3a0cd75174aba3ec0b01962d507300e9e7
Andrea <andry.tino@gmail.com>
2016-10-09 19:37:27 +0200

    Revert "First commit"
    This reverts commit 27b417550adfd3f51acfc12bf0132c0d2e51467a.

--- shifu/hello-world/hello-world-II.js
+++
@@ -1,8 +0,0 @@
-/**
- * hello-world-II.js
- * Andrea Tino - 2016
- */
-
-window.addEventListener("load", function() {
-    document.body.textContent = "Hello World!";
-});
--- shifu/variables/hello-world.js
+++
@@ -1,9 +0,0 @@
-/**
- * variables.js
- * Andrea Tino - 2016
- */
-
-window.addEventListener("load", function() {
-    var text = prompt("Type your favorite greeting"); // User input
-    document.body.textContent = text;
-});
